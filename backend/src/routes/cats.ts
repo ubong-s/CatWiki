2f5845952f399ca6751c801bd227f61758d60742
Ubong Sylvester <ubongsly@gmail.com>
2022-12-02 19:46:21 +0100

    completed fetching of single cat

--- backend/src/routes/cats.ts
+++ backend/src/routes/cats.ts
@@ -17,6 +17,27 @@ catsRouter.get('/', async (_request, response) => {
    }
 });
 
+catsRouter.get('/:id', async (request, response) => {
+   const {
+      params: { id },
+   } = request;
+
+   try {
+      const res = await fetch(
+         `${process.env.BASE_API_URL}/breeds/${id}` as string
+      );
+      const data: CatProps = await res.json();
+
+      if (Object.keys(data).length < 1) {
+         return response.status(404).json({ error: 'No such cat exists' });
+      } else {
+         return response.status(200).json(data);
+      }
+   } catch (error) {
+      return response.status(400).json({ error: 'Error fectching' });
+   }
+});
+
 catsRouter.get('/search', async (request, response) => {
    const { body } = request;
 
